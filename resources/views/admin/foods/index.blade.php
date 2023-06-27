3229a09c94a8f82af3d29f074ed524b501070f07
Paolo Giovannini <giovannini13@gmail.com>
2023-06-27 15:46:53 +0200

    rimossi commenti layout precedente

--- resources/views/admin/foods/index.blade.php
+++ resources/views/admin/foods/index.blade.php
@@ -38,30 +38,5 @@
             <p>Non ci sono cibi</p>
             <a class="btn btn-primary" href="{{ route('admin.dashboard') }}">Vai alla dashboard</a>
         @endforelse
-        {{-- <ul>
-            @forelse ($foods as $food)
-                <li>
-                    <p>{{ $food->name }}</p>
-                    <div class="d-flex">
-                        <a class="btn rounded-pill btn-primary me-2"
-                            href="{{ route('admin.foods.show', $food->slug) }}">VEDI</a>
-                        <a href="{{ route('admin.foods.edit', $food->slug) }}"
-                            class="btn rounded-pill btn-warning me-2">MODIFICA</a>
-                        <form action="{{ route('admin.foods.destroy', ['food' => $food->slug]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn rounded-pill btn-danger">ELIMINA</button>
-                        </form>
-                    </div>
-
-
-                </li>
-
-            @empty
-                <p>Non ci sono cibi</p>
-                <a class="btn btn-primary" href="{{ route('admin.dashboard') }}">Vai alla dashboard</a>
-            @endforelse
-
-        </ul> --}}
     </div>
 @endsection
