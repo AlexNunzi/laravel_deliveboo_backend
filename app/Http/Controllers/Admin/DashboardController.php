8e25b0be5e7c018ee3d1d11bae41ed60db034ddc
Alex Nunzi <alex.nunzi@hotmail.it>
2023-06-28 12:51:55 +0200

    Passo la palla a Giulia

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,33 +8,49 @@ use App\Models\Restaurant;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $restaurant = Restaurant::select('id')->where('user_id', Auth::id())->first();
-        $restaurants_id = $restaurant->id;
 
         $date = new DateTime();
         $lastDay = $date->format('Y-m-t');
         $dateMinus12 = $date->modify('-11 months')->format('Y-m-01');
 
-        $ordersRevenue = Order::where('id', $restaurants_id)
-            ->whereBetween('created_at', [$dateMinus12, $lastDay])
+        $ordersRevenue = Order::with('food')->whereHas('food', function (Builder $query) use ($restaurant) {
+            $query->where('restaurant_id', $restaurant->id);
+        })->whereBetween('created_at', [$dateMinus12, $lastDay])
             ->selectRaw("SUM(total_price) AS total")
             ->selectRaw("DATE_FORMAT(created_at, '%Y-%m') AS date")
             ->groupby("date")
-            ->orderBy("date", "DESC")
-            ->get();
+            ->orderBy("date", "DESC")->get();
 
-        $ordersCount = Order::where('id', $restaurants_id)
-            ->whereBetween('created_at', [$dateMinus12, $lastDay])
+        $ordersCount = Order::with('food')->whereHas('food', function (Builder $query) use ($restaurant) {
+            $query->where('restaurant_id', $restaurant->id);
+        })->whereBetween('created_at', [$dateMinus12, $lastDay])
             ->selectRaw("COUNT(id) AS orderTotal")
             ->selectRaw("DATE_FORMAT(created_at, '%Y-%m') AS date")
             ->groupby("date")
-            ->orderBy("date", "DESC")
-            ->get();
+            ->orderBy("date", "DESC")->get();
+
+        // $ordersRevenue = Order::where('id', $restaurants_id)
+        //     ->whereBetween('created_at', [$dateMinus12, $lastDay])
+        //     ->selectRaw("SUM(total_price) AS total")
+        //     ->selectRaw("DATE_FORMAT(created_at, '%Y-%m') AS date")
+        //     ->groupby("date")
+        //     ->orderBy("date", "DESC")
+        //     ->get();
+
+        // $ordersCount = Order::where('id', $restaurants_id)
+        //     ->whereBetween('created_at', [$dateMinus12, $lastDay])
+        //     ->selectRaw("COUNT(id) AS orderTotal")
+        //     ->selectRaw("DATE_FORMAT(created_at, '%Y-%m') AS date")
+        //     ->groupby("date")
+        //     ->orderBy("date", "DESC")
+        //     ->get();
 
         return view('admin.dashboard', compact('ordersRevenue', 'ordersCount', 'restaurant'));
     }
